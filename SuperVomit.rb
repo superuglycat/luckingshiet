572fb9f8a7148c762584e55e716f523979cd7478
Ugly CAT <53712265+superuglycat@users.noreply.github.com>
2019-10-12 09:04:20 +0200

    UltraVomit
    Ofc it will be the ultimate version of this.

--- SuperVomit.rb
+++ SuperVomit.rb
@@ -1,47 +1,58 @@
-class SuperVomi
-  attr_reader :list
+=begin
+  This is my PhD project.
+  You'll find more informations right there:
+  https://youtube.com/watch?v=fOEjSgAiluk
 
-  def initialize(*args)
-    @list = args
-  end
-
-  def short(list, suffix)
-    list.sort!
-    arr2 = []
-    list.each{|k| arr2 << k.delete_suffix(suffix)}
-    arr2 = arr2.join
-    arr2 << "phile"
-  return arr2
-  end
-
-  def generate(list = self.list)
-    new_arr = []
-
-    for i in (0..(list.size)-1)
-      z = rand(2)
-      new_arr += [list[i]] if z == 1
-    end
-    short(new_arr, "phile")
-  end
-
-end
+  Instructive comments by Richard Stallman.
+=end
 
 
-# Runtime
-testinglist = ['zoophile', 'necrophile', 'pedophile', 'flatuphile', 'alektotophile', 'pnigophile', 'emetophile', 'scatophile']
-vomi = SuperVomi.new(testinglist)
+class SuperVomi # * ... HAI DIDDLE DEE DEE
+  attr_reader :list # * ... C'est tous les jours Noël !
 
-loop do 
-  # * ... intuitive prompt
-  p "Combien dois-je generer de vomi ?"
-  number_of_vomi = gets
+  def initialize(*args) # * ... Look at mah vomit
+    @list = args.flatten.uniq # * ... Mah vomit is amazing. Give it a lick.
+  end # * ... Yuuum! It tastes like raisin!!
 
-  if number_of_vomi =~ /[[:digit:]]/
-    # * ... generate vomits
-    number_of_vomi.to_i.times {p vomi.generate}
-    break
-  else
-    p "ERREUR: Tu dois me dire combien de fois je vomis !!!"
-    p "ERRORCODE: 0x218394: y o u ' r e  n o t  r e a d y  t o  u s e  t h e  v o m i t  y e t" 
-  end
-end
+  def short(list, suffix) # * ... Oh lala, j'meurs de faim.
+    list.sort! # * ... Je me demande ce qu'il y a pour dîner.
+    arr2 = [] # * ... Dîner.
+    list.each{|k| arr2 << k.delete_suffix(suffix)} # * ... Dîner.
+    arr2 = arr2.join # * ... Dîner.
+    arr2 << "phile" # * ... Dîner.
+  return arr2 # * ... Dîner.
+  end # * ... Dîner.
+
+  def generate(list = self.list) # * ... Hijo de puta malparido
+    new_arr = [] # * ... Me cago en la leche que mamaste.
+
+    for i in (0..(list.size)-1) # * ... La gruesa semen de tu padre.
+      z = rand(2) # * ... Coma mierda.
+      new_arr += [list[i]] if z == 1 # * ... Coma la gruesa semen.
+    end # * ... BIBIIIITE
+    short(new_arr, "phile") # * ... JE ME DEMANDE CE QU'IL Y A POUR DÎNER
+  end # * ... GANON
+
+end # * ... CE SONT... LES FORCES DU MAL !!
+
+
+# * ... OH NON PHILIPPE G ENKOR PT
+pipicaca = ['zoophile', 'necrophile', 'pedophile', 'flatuphile', 'alektotophile', 'pnigophile', 'emetophile', 'scatophile'] # * ... TRAU DAILIR
+vomit = SuperVomi.new(pipicaca) # ... PROUT
+
+loop do # * ... FHJDSTJCRTHKLLHFDGJ'
+  p "Combien dois-je generer de vomi ?" # * ... I REALLY LIKE YOU
+  zizi = gets # * ... BE MY PRIVATE DANCER
+
+  if zizi =~ /[[:digit:]]/ # * ... WUBWUBWUBWUB
+    # * ... generate vomits # * ... DON'T STARE AT THE FUCKING CHAIR
+    zizi.to_i.times {p vomit.generate} # * ... BUUUURP
+    break # * ... TOUT CASSER
+  else # * ... AAAAAAAAAAAHHHHH
+    p "ERREUR: Tu dois me dire combien de fois je vomis !!!" # * ... J'AIME LES FRITENT
+    p "ERRORCODE: 0x218394: y o u ' r e  n o t  r e a d y  t o  u s e  t h e  v o m i t  y e t" # * ... JE FÉ KK
+  end # * ... DAN MÉ DRA
+end # * ... JE FÉ PIPI DAN MON LI
+
+
+# * ... THIS TOTAL SHIET IS A TRANSPOSITION OF WHAT WOULD BE SKRILLEX IN RUBY
